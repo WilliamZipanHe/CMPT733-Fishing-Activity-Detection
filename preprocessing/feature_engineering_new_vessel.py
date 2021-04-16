e63d78c41f9005233930fa5a767a41fd312b2451
C.Y. Law <cannonbunny@users.noreply.github.com>
2021-04-15 17:25:58 -0700

    add comment to notebook and reorganize files

---
+++ preprocessing/feature_engineering_new_vessel.py
@@ -0,0 +1,78 @@
+import sys
+assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
+
+from PIL import Image
+import pandas as pd
+import numpy
+
+
+# this code is moved to scraping/scape_preprocess.py. 
+# However, the slice data function should still be used to cut down the data size before uploading to s3
+
+# generate image file and convert to df
+Image.MAX_IMAGE_PIXELS = None
+
+im = Image.open('../data/distance-from-shore.tif')
+im_array = numpy.array(im)
+d_2_shore = pd.DataFrame(im_array)
+
+im2 = Image.open('../data/distance-from-port-v1.tiff')
+im_array2 = numpy.array(im2)
+d_2_port = pd.DataFrame(im_array2)
+
+# map shore distance to lat/lon
+def d2s_function(x):
+    num = d_2_shore.loc[round(x['lat']*100 + 9000)][round(x['lon']*100 + 18000)]
+    return num
+
+# map port distance to lat/lon
+def d2p_function(x):
+    num = d_2_port.loc[round(x['lat']*100 + 9000)][round(x['lon']*100 + 18000)]
+    return num
+
+# slice shore and port distance to smaller set to upload to s3 for reallife data (used only once)
+def slice_data(df1, df2):
+    df1 = df1.loc[4750+9000:5000+9000, -12750+18000:-12270+18000]
+    df1.to_csv('dfs_bc_section.csv')
+    df2 = df2.loc[4750+9000:5000+9000, -12750+18000:-12270+18000]
+    df2.to_csv('dfs_bc_section.csv')
+    sst_cut = pd.read_csv('../data/sst_grid.csv', index_col=0)
+    sst_cut["time_bnds"] = pd.to_datetime(sst_cut["time_bnds"]).dt.to_period('M')
+    sst_cut = sst_cut[(sst_cut['lat'] > 45) & (sst_cut['lat'] < 50) & (sst_cut['lon'] > -130) & \
+                      (sst_cut['lon'] < -125) & (sst_cut['time_bnds'].dt.year == 2015)]
+    sst_cut.to_csv('sst_grid_sm.csv', index = False)
+    precip_cut = pd.read_csv('../data/precip_grid.csv', index_col=0)
+    precip_cut["time"] = pd.to_datetime(precip_cut["time"]).dt.to_period('M')
+    precip_cut = precip_cut[(sst_cut['lat'] > 45) & (precip_cut['lat'] < 50) & (precip_cut['lon'] > -130) & \
+                      (precip_cut['lon'] < -125) & (precip_cut['time'].dt.year == 2015)]
+    precip_cut.to_csv('precip_grid_sm.csv', index = False)
+
+# setting general
+def main(input_path, vessel_name):
+    df = pd.read_csv('../data/' + input_path)
+
+    # getting distance data into df
+    df['distance_from_shore'] = df.apply(lambda x: d2s_function(x), axis=1)
+    df['distance_from_port'] = df.apply(lambda x: d2p_function(x), axis=1)
+
+    # manually set source and gear data type
+    df['source_crowd_sourced'] = 0
+    df['source_dalhousie_longliner'] = 0
+    df['source_dalhousie_ps'] = 0
+    df['source_dalhousie_trawl'] = 0
+    df['source_false_positives'] = 0
+    df['source_gfw'] = 0
+    df['gear_type_drifting_longlines'] = 0
+    df['gear_type_fixed_gear'] = 0
+    df['gear_type_pole_and_line'] = 0
+    df['gear_type_purse_seines'] = 0
+    df['gear_type_trawlers'] = 1
+    df['gear_type_trollers'] = 0
+
+    df.to_csv('../data/' + vessel_name + '.csv', index = False)
+
+
+if __name__ == '__main__':
+    input_path = sys.argv[1]
+    vessel_name = sys.argv[2]
+    main(input_path, vessel_name)
---
+++ preprocessing/preprocessing.py
@@ -0,0 +1,74 @@
+import sys
+assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
+
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import joblib
+
+
+# fill missing values
+def preprocess_missing_value(df):
+    df['precip'] = df['precip'].fillna(0)
+    df['sst'] = df['sst'].fillna(0)
+    df['speed_rolling_mean'] = df['speed_rolling_mean'].bfill()
+    df['course_rolling_mean'] = df['course_rolling_mean'].bfill()
+    df['speed_rolling_mean_3h'] = df['speed_rolling_mean_3h'].bfill()
+    df['course_rolling_mean_3h'] = df['course_rolling_mean_3h'].bfill()
+    df['course'] = df.groupby(['mmsi'])['course'].ffill().bfill()
+    df['speed'] = df.groupby(['mmsi'])['speed'].ffill().bfill()
+    return df
+
+# one hot encode string params
+def preprocess_one_hot(df):
+    df = pd.get_dummies(df, columns = ['hemisphere_ns'])
+    df = pd.get_dummies(df, columns = ['hemisphere_ew'])
+
+    if 'hemisphere_ns_north' not in df:
+        df['hemisphere_ns_north'] = 0
+    if 'hemisphere_ns_south' not in df:
+        df['hemisphere_ns_south'] = 0
+    if 'hemisphere_ew_east' not in df:
+        df['hemisphere_ew_east'] = 0
+    if 'hemisphere_ew_west' not in df:
+        df['hemisphere_ew_west'] = 0
+
+    # comment out for real life data--------------
+    df = pd.get_dummies(df, columns = ['source'])
+    df = pd.get_dummies(df, columns = ['gear_type'])
+    return df
+
+# scale all params and save scaler
+def preprocess_scaling(df, scaler):
+    # comment out for real life data--------------
+    data = df.iloc[:, 1:]
+    df[data.columns] = scaler.fit_transform(data)
+    joblib.dump(scaler, 'scaler.gz')
+    # comment out for training data--------------
+    # df = df[['lat_x','lon_x','course','speed','timestamp','distance_from_shore','distance_from_port','mmsi','doy','dow','season','year','month','day','hour',\
+    #     'sst','precip','speed_expand_mean','course_expand_mean','speed_rolling_mean','course_rolling_mean','speed_rolling_mean_3h','course_rolling_mean_3h',\
+    #     'speed_rolling_mean_12h','course_rolling_mean_12h','speed_rolling_mean_1d','course_rolling_mean_1d','speed_rolling_mean_3d',\
+    #     'course_rolling_mean_3d','source_crowd_sourced','source_dalhousie_longliner','source_dalhousie_ps','source_dalhousie_trawl',\
+    #     'source_false_positives','source_gfw','gear_type_drifting_longlines','gear_type_fixed_gear','gear_type_pole_and_line','gear_type_purse_seines',\
+    #     'gear_type_trawlers','gear_type_trollers','hemisphere_ns_north','hemisphere_ns_south','hemisphere_ew_east','hemisphere_ew_west'
+    # ]]
+    # scaler = joblib.load('scaler.gz')
+    # df[df.columns] = scaler.transform(df)
+    #---------------------------------------------
+    return df
+
+def main(input_path, version):
+    df = pd.read_csv('../data/'+ input_path)
+    scaler = MinMaxScaler()
+    del df['adjust_time_date']
+    df = preprocess_missing_value(df)
+    df = preprocess_one_hot(df)
+    df = preprocess_scaling(df, scaler)
+    print(df)
+    df.to_csv('../data/preprocess_dataset_' + version + '.csv', index=False)
+
+
+if __name__ == '__main__':
+    input_path = sys.argv[1]
+    version = sys.argv[2]
+    main(input_path, version)
