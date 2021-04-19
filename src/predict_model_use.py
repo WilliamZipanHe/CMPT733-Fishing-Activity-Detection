d060db497eb55025df11b6718de891061de595c4
Zipan He <73502151+WilliamZipanHe@users.noreply.github.com>
2021-04-18 23:44:17 -0700

    Update predict_model_use.py

--- src/predict_model_use.py
+++ src/predict_model_use.py
@@ -106,7 +106,7 @@ def bo_lgb_train(opt, x_train, y_train, x_test, y_test):
 
 
 if __name__=="__main__":
-    df = pd.read_csv(os.path.join('..', 'data', 'preprocess_dataset_v1.csv'))
+    df = pd.read_csv(os.path.join('..', 'data', 'preprocess_dataset_v2.csv'))
     df_1 = df.drop('is_fishing', axis = 1)
     x_train, x_test, y_train, y_test = train_test_split(df_1, df['is_fishing'], test_size=0.2, random_state=42)
     #n_iters could be changed for baeysian optimization iteration times
